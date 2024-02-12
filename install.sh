7e5b789d6add70cef50936d02ab5c09b8ad9d29d
Erdemina <erdem.uluta@gmail.com>
2024-02-12 16:41:00 +0300

    a

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git
 yay -S nwg-look-bin
 yay -S visual-studio-code-bin
 yay -S wlogout
+yay -S xorg-xhost
+xhost +SI:localuser:root
 git clone https://github.com/vinceliuice/Fluent-gtk-theme.git && ./Fluent-gtk-theme/install.sh
 wget https://github.com/dracula/gtk/archive/master.zip && unzip -d ~/.themes/ master.zip && mv ~/.themes/gtk-master ~/.themes/Dracula &&  rm -rf master.zip
 git clone https://aur.archlinux.org/sddm-theme-tokyo-night.git && cd sddm-theme-tokyo-night && makepkg -Ccsi
