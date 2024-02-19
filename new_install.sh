38f99a86a62e3c48a499fef7dcd75ba0eebeabcc
Erdemina <erdem.uluta@gmail.com>
2024-02-19 23:53:01 +0300

    added zsh to yay

--- new_install.sh
+++ new_install.sh
@@ -1,6 +1,6 @@
 cp -r rofi/ hypr/ waybar/ alacritty/ ~/.config/
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S rofi hyper visual-studio-code-bin alacritty nautilus pavucontrol waybar hyprpaper hyprshot wlogout nwg-look-bin nautilus hyprshot unzip brightnessctl xsensors
+yay -S rofi hyper zsh visual-studio-code-bin alacritty nautilus pavucontrol waybar hyprpaper hyprshot wlogout nwg-look-bin nautilus hyprshot unzip brightnessctl xsensors
 git clone https://github.com/vinceliuice/Fluent-gtk-theme.git && ./Fluent-gtk-theme/install.sh
 rm -rf ./Fluent-gtk-theme
 git clone https://aur.archlinux.org/sddm-theme-tokyo-night.git && cd sddm-theme-tokyo-night && makepkg -Ccsi
