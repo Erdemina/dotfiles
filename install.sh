141cb6b9a70fe80aa15f3b8b3f869558373fe948
Erdemina <erdem.uluta@gmail.com>
2024-05-01 18:25:52 +0300

    Test

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
-sudo pacman -R pulse-native-provider
-
-sudo pacman -S pulseaudio vlc waybar otf-font-awesome ttf-jetbrains-mono alacritty rofi nautilus hyprpaper git
-sudo pacman -S pavucontrol unzip hyprshot brightnessctl mpd mpc xsensors
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S nwg-look-bin
-yay -S visual-studio-code-bin
-yay -S wlogout
-yay -S xorg-xhost
-xhost +SI:localuser:root
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+../
+yay -S rofi otf-font-awesome ttf-jetbrains-mono  hyper  zsh  bluez bluez-utils blueman visual-studio-code-bin alacritty pavucontrol waybar hyprpaper hyprshot  nwg-look-bin nautilus hyprshot unzip brightnessctl xsensors
+cp -r rofi/ hypr/ waybar/ alacritty/ ~/.config/
 git clone https://github.com/vinceliuice/Fluent-gtk-theme.git && ./Fluent-gtk-theme/install.sh
-wget https://github.com/dracula/gtk/archive/master.zip && unzip -d ~/.themes/ master.zip && mv ~/.themes/gtk-master ~/.themes/Dracula &&  rm -rf master.zip
+rm -rf ./Fluent-gtk-theme
 git clone https://aur.archlinux.org/sddm-theme-tokyo-night.git && cd sddm-theme-tokyo-night && makepkg -Ccsi
+cd ../ && rm -rf sddm-theme-tokyo-night
+sudo cp -r ./etc/* /etc/ 
